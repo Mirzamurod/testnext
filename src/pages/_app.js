b627f33d20205a120a754dfde9108b24ffde539d
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2022-12-22 21:42:57 +0500

    test next 3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import LoadingScreen from '../components/shared/Loading/loadingscreen';
-import '../styles/globals.css';
+import React, { useEffect, useState } from 'react'
+import LoadingScreen from '../components/shared/Loading/loadingscreen'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    setLoading(true);
-  }, []);
-  useEffect(() => {
-    setTimeout(() => setLoading(true), 6000);
-  })
-  return (
-    <>
-      {!loading ? (<LoadingScreen />):(<Component {...pageProps} />)}
-    </>
-  );
+    useEffect(() => {
+        setLoading(true)
+    }, [])
+
+    useEffect(() => {
+        setTimeout(() => setLoading(true), 6000)
+    })
+
+    return !loading ? <LoadingScreen /> : <Component {...pageProps} />
 }
 
 export default MyApp
